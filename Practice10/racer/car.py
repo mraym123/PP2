00f0529be97c5df41c042f49683151ef6e61f803
mraym123 <r_maipasov@kbru.kz>
2026-04-23 20:58:38 +0500

    Add practice10

---
+++ Practice10/racer/car.py
@@ -0,0 +1,59 @@
+import pygame
+import random
+from pygame.locals import *
+
+# Constants
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, speed):
+        super().__init__() 
+        # Path updated to look in mater folder
+        self.image = pygame.image.load("mater/Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
+        self.speed = speed
+
+    def move(self, current_speed):
+        self.rect.move_ip(0, current_speed)
+        if (self.rect.top > SCREEN_HEIGHT):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+            return True 
+        return False
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        # Path updated to look in mater folder
+        self.image = pygame.image.load("mater/Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+       
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+                  
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # Path updated to look in mater folder
+        self.image = pygame.image.load("mater/coin.png")
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self, speed):
+        self.rect.move_ip(0, speed)
+        if (self.rect.top > SCREEN_HEIGHT):
+            self.spawn()
+
+    def spawn(self):
+        self.rect.top = 0
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
